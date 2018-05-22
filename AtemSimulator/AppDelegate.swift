066319eefc8b7cdcec9f6e5fcfa6610e00bcc56c
Damiaan Dufaux <damiaan@dpro.be>
2018-05-22 14:16:48 +0200

    Initial Commit

---
+++ AtemSimulator/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  AtemSimulator
+//
+//  Created by Damiaan on 22/05/18.
+//  Copyright © 2018 Devian. All rights reserved.
+//
+
+import Cocoa
+import Atem
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ AtemSimulator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AtemSimulator
+//
+//  Created by Damiaan on 22/05/18.
+//  Copyright © 2018 Devian. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
